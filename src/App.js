a467fde99c89f3642ab06174373e4c94124da58d
Shariq512000 <www.shariq512000@gmail.com>
2023-06-18 14:07:40 -0700

    incomplete

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import Sidebar from './components/Side Bar/SideBar';
+import MyProSidebar from './components/Left Bar/LeftBar';
+import { MyProSidebarProvider } from './components/Left Bar/sidebarContext';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      {/* <MyProSidebarProvider> */}
+
+      <Sidebar />
+      {/* <MyProSidebar /> */}
+      {/* </MyProSidebarProvider> */}
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
